d66ae290862aebdddd874b77d4db327d901090e0
Bohdan <salabay2003@gmail.com>
2023-05-31 18:46:56 +0300

    Add web app implementation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from tensorflow.keras.models import load_model
 import tensorflow as tf
+# from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
 app = Flask(__name__)
@@ -11,9 +12,13 @@ CORS(app)
 def hello():
     model = load_model("model.h5")
     data = request.get_json()
+#     non_numeric_cols = ["rcount", "hematocrit", "neutrophils", "sodium", "glucose", "bloodureanitro", "creatinine", "bmi",
+#                         "pulse", "respiration"]
+#     scaler = MinMaxScaler()
+#     data[non_numeric_cols] = scaler.fit_transform(data[non_numeric_cols])
     data = np.array(data, dtype=np.float32)
     data = tf.reshape(data, (data.shape[0], 1, data.shape[1]))
-    res = model.predict(data, verbose=0)
+    res = np.round(model.predict(data, verbose=0))
     return res[0].tolist()
 
 if __name__ == '__main__':
--- script.js
+++ script.js
@@ -1,25 +1,102 @@
-const data = [[0, 0.38071066, 0.03783564, 0.46541822, 0.54682434, 0.01614087,
-    0.5397044, 0.41830018, 0.5321101, 0.64285713, 0, 1,
-    1, 0, 1, 0, 1, 0,
-    1, 0, 1, 0, 1, 0,
-    1, 0, 1, 0, 1, 0,
-    1, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0,
-    0, 0, 0]];
+// const data = [[0, 1, 0, 0, 0, 0,
+//     0, 1, 1, 0, 0, 0,
+//     0, 0.49238577, 0.0374288, 0.66779757, 0.5652498, 0.01907557,
+//     0.4643127,  0.5987951, 0.49541286, 0.63265306, 4]];
+//
+// const jsonData = JSON.stringify(data);
 
-const jsonData = JSON.stringify(data);
+const btn = document.getElementById("form-button");
 
-fetch('http://127.0.0.1:5000', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: jsonData
-})
-    .then(response => response.json())
-    .then(result => {
-        console.log(result);
+function getRadioValue (radioName) {
+    const radios = document.getElementsByName(radioName);
+    for (let i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            return Number(radios[i].value)
+        }
+    }
+}
+
+btn.onclick = function() {
+    const e = document.getElementById("rcount");
+    const rcount = Number(e.options[e.selectedIndex].value);
+    const gender = getRadioValue("gender");
+    const dialysisrenalendstage = getRadioValue("dialysisrenalendstage");
+    const asthma = getRadioValue("asthma");
+    const irondef = getRadioValue("irondef");
+    const pneum = getRadioValue("pneum");
+    const substancedependence = getRadioValue("substancedependence");
+    const psychologicaldisordermajor = getRadioValue("psychologicaldisordermajor");
+    const depress = getRadioValue("depress");
+    const psychother = getRadioValue("psychother");
+    const fibrosisandother = getRadioValue("fibrosisandother");
+    const malnutrition = getRadioValue("malnutrition");
+    const hemo = getRadioValue("hemo");
+    const hematocrit = Number(document.getElementById("hematocrit").value);
+    const neutrophils = Number(document.getElementById("neutrophils").value);
+    const sodium = Number(document.getElementById("sodium").value);
+    const glucose = Number(document.getElementById("glucose").value);
+    const bloodureanitro = Number(document.getElementById("bloodureanitro").value);
+    const creatinine = Number(document.getElementById("creatinine").value);
+    const bmi = Number(document.getElementById("bmi").value);
+    const pulse = Number(document.getElementById("pulse").value);
+    const respiration = Number(document.getElementById("respiration").value);
+    const secondarydiagnosisnonicd9 = Number(document.getElementById("secondarydiagnosisnonicd9").value);
+
+    const data = [[
+        rcount,
+        gender,
+        dialysisrenalendstage,
+        asthma,
+        irondef,
+        pneum,
+        substancedependence,
+        psychologicaldisordermajor,
+        depress,
+        psychother,
+        fibrosisandother,
+        malnutrition,
+        hemo,
+        hematocrit,
+        neutrophils,
+        sodium,
+        glucose,
+        bloodureanitro,
+        creatinine,
+        bmi,
+        pulse,
+        respiration,
+        secondarydiagnosisnonicd9
+    ]];
+
+    const jsonData = JSON.stringify(data);
+
+    fetch('http://127.0.0.1:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: jsonData
     })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        .then(response => response.json())
+        .then(result => {
+            document.getElementById("result").textContent = "Predicted length of stay at hospital is: " + result + " day(s)";
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+};
+
+// fetch('http://127.0.0.1:5000', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: jsonData
+// })
+//     .then(response => response.json())
+//     .then(result => {
+//         console.log(result);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
